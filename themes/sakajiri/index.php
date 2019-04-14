1a1259450151876b95c0bc8c2b92aba90d81aa50
cha1ra <cha1ra.garnet@gmail.com>
2019-04-15 03:29:45 +0900

    first commit

---
+++ themes/sakajiri/index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title><?php bloginfo('name'); ?>さかじり音楽教室 - 千葉県柏市のピアノ・エレクトーン教室</title>
+  <meta name="description" content="<?php bloginfo('description'); ?>" />
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/reset.css">
+  <link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c" rel="stylesheet">
+  <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+</head>
+<body>
+  <header>
+    <div class="header-logo-area">
+      <img 
+        src="<?php bloginfo('template_url'); ?>/assets/img/logo.png" 
+        alt="さかじり音楽教室"
+        class="header-logo-area-img" 
+        />
+      <h1>ヤマハjet公認の千葉県柏市のピアノ・エレクトーン教室 | 子供から大人まで♪</h1>
+    </div>
+    <nav>
+      <ul>
+        <li><a href="#info-area">お知らせ</a></li>
+        <li><a href="#about-area">ご挨拶</a></li>
+        <li><a href="#lesson-area">レッスン</a></li>
+        <li><a href="#application-area">体験/入会</a></li>
+        <li><a href="#application-area">アクセス</a></li>
+      </ul>
+    </nav>
+  </header>
+  <section id="movie">
+    <iframe 
+      width="100%" 
+      height="auto" 
+      src="https://www.youtube.com/embed/iADEN2flzwE" 
+      frameborder="0" 
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+      allowfullscreen
+      >
+    </iframe>
+  </section>
+  <section id="info-area">
+    <div class="container">
+      <h2><span>教室からのお知らせ<span></h2>
+      <h3><span>直近開催のイベント</span></h3>
+      <div class="event">
+        <div class="event-card">
+          <p class="evnet-date">2019.99.99(土)</p>
+          <p class="evnet-description">JoyfulConcert 2019</p>
+        </div>
+        <div class="event-card">
+          <p class="event-date">2019.99.99(土)</p>
+          <p class="event-description">Gift. </p>
+        </div>
+      </div>
+      <h3><span>教室通信</span></h3>
+      <div class="blog">
+        <div class="blog-card">
+          <p class="blog-date">2019.99.99(土)</p>
+          <p class="blog-description">Joyful Concertが無事に終了しました♪</p>
+        </div>
+        <div class="blog-card">
+          <p class="blog-date">2019.99.99(土)</p>
+          <p class="blog-description">クリスマス会を開催しました♪</p>
+        </div>
+        <div class="blog-card">
+          <p class="blog-date">2019.99.99(土)</p>
+          <p class="blog-description">クリスマス会を開催しました♪</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section id="about-area">
+    <h2><span>講師ご挨拶</span></h2>
+    <div 
+      class="parallax-window" 
+      data-parallax="scroll" 
+      data-speed="0.6" 
+      data-position="center top" 
+      data-image-src="http://sakajirimusic.sakura.ne.jp/wp/wp-content/uploads/2018/05/Snapseed2.jpg"
+      data-natural-width="300" 
+      data-natural-height="300"
+      >
+      <div class="space"></div>
+      <div class="container">
+        <div class="intro">
+          <div class="container">
+            <p>
+              音を楽しむことの素晴らしさを伝えていきたい…
+            </p>
+            <p>そんな想いからはじまったさかじり音楽教室もおかげ様で35周年を迎えました。</p>
+            <p>今もなお３０名を超える生徒さんに囲まれながら楽しい日々を過ごせていることに感謝しています。</p>
+            <p>生徒さんに負けないよう、私自身もピアノの先生としてだけではなく、一音楽家としても精力的に活動していきます。</p>
+            <p>これからもさかじり音楽教室を末永くよろしくお願いします。</p>
+            <p>2019年4月15日 坂尻くにえ</p>
+          </div>
+        </div>
+        <div class="profile">
+          <h3>坂尻くにえ 講師略歴</h3>
+          <div class="container">
+            <img 
+              src="http://sakajirimusic.sakura.ne.jp/wp/wp-content/uploads/2018/05/30726975_950622678433620_3720951123548877061_n-300x297.jpg" 
+              alt="坂尻くにえ"
+              class="profile-img"
+              >
+            <div class="profile-desc">
+              <p>上野学園大学音楽学部器楽学科ピアノ科卒業。各種演奏会に首席として出演。ヤマハ銀座店「永年功労賞」受賞。</p>
+              <p>詳しいプロフィールはこちら</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="space"></div>
+    </div>
+  </section>
+  <section id="lesson-area">
+    <div class="container">
+      <h2><span>レッスン内容</span></h2>
+      <div class="info-card">
+        <p class="info-date">9999.99.99</p>
+        <p class="info-description">hogehogehogehoge</p>
+      </div>
+    </div> <!-- End of Parallax Window Div -->
+  </section>
+  <section id="application-area">
+    <div class="container">
+      <h2><span>体験レッスン / ご入会</span></h2>
+      <h3>まずは無料体験レッスンから！</h3>
+      <p>ピアノ・エレクトーンを始めようと考えているなら、まずは無料体験レッスンから♪是非教室の雰囲気を感じてください。</p>
+      <p>レッスンの方法や指導方針など、みなさまの疑問や不安に、きめこまかくお応えします。</p>
+    </div>  
+  </section>
+  <section id="access-area">
+    <div class="container">
+      <h2><span>アクセス</span></h2>
+      <p>〒277-0862 千葉県柏市篠籠田1036-5</p>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3233.315081653325!2d139.9489805148633!3d35.86579472743588!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60189db8f766e71b%3A0xa1e5e2a2d55063d7!2z44GV44GL44GY44KK6Z-z5qW95pWZ5a6k!5e0!3m2!1sja!2sjp!4v1555254612245!5m2!1sja!2sjp" width="100%" height="100" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </div>  
+  </section>
+
+
+
+  <footer>
+    <ul>
+      <li>さかじり音楽教室 トップ</li>
+      <li>特徴</li>
+      <li>コース</li>
+      <li>公式Facebook</li>
+    </ul>
+  </footer>
+
+
+  <!-- Script -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/assets/js/scroll.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/assets/js/parallax.min.js"></script>
+  
+</body>
+</html>
